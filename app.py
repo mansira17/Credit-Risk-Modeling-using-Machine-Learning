52bf548c8f76062719e0c4fc3781c9f08ad709fc
mansi_ra <73093538+mansira17@users.noreply.github.com>
2025-09-25 13:59:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -48,9 +48,9 @@ age = st.number_input('Age', min_value = 18, max_value = 80, value = 30)
 sex = st.selectbox('Sex', ['male', 'female'])
 job = st.number_input('Job (0 - 3)', min_value = 0, max_value = 3, value = 1)
 housing = st.selectbox('Housing', ['own', 'rent', 'free'])
-saving_accounts = st.selectbox('Saving Accounts', ['little', 'moderate', 'rich', 'quite rich'])
-checking_account = st.selectbox('Checking Account', ['moderate', 'little', 'rich'])
-credit_amount = st.number_input('Credit Amount', min_value = 0, value = 100)
+saving_accounts = st.selectbox('Saving accounts', ['little', 'moderate', 'rich', 'quite rich'])
+checking_account = st.selectbox('Checking account', ['moderate', 'little', 'rich'])
+credit_amount = st.number_input('Credit amount', min_value = 0, value = 100)
 duration = st.number_input('Duration (months)', min_value=1, value = 12)
 
 
@@ -62,9 +62,9 @@ input_df = pd.DataFrame({
     'Sex' : [encoders['Sex'].transform([sex])[0]],
     'Job' : [job],
     'Housing' : [encoders['Housing'].transform([housing])[0]],
-    'Saving Accounts' : [encoders['Saving accounts'].transform([saving_accounts])[0]],
-    'Checking Account' : [encoders['Checking account'].transform([checking_account])[0]],
-    'Credit Amount' : [credit_amount],
+    'Saving accounts' : [encoders['Saving accounts'].transform([saving_accounts])[0]],
+    'Checking account' : [encoders['Checking account'].transform([checking_account])[0]],
+    'Credit amount' : [credit_amount],
     'Duration' : [duration]
 })
 
